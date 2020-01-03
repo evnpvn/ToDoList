53ebe29b0a4eb8e5a4573c949f0bd63b0b9154ff
evan <evan@evanpavan.com>
2020-01-03 09:55:14 -0500

    Fixed bugs with lowest/highest priorities not printing the right message

--- Tasklist.cs
+++ Tasklist.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using static System.Console;
 
 namespace ToDoList
 {
@@ -9,8 +10,6 @@ namespace ToDoList
         {   
             string taskItem = this[index];
             
-
-            //FIXME: currently if you move the last item in the list lower if drops the item from the list
             if(prioritySetting == "highest")
             {
                 this.RemoveAt(index);
@@ -19,25 +18,36 @@ namespace ToDoList
             else if(prioritySetting == "lowest")
             {
                 this.RemoveAt(index);
-                this.Insert(this.Count - 1, taskItem); 
+                this.Insert(this.Count, taskItem); 
             }
-            else if(prioritySetting == "higher" && this.IndexOf(taskItem) != 0)
+            else if(prioritySetting == "higher")
             {
-                this.RemoveAt(index);
-                this.Insert(index - 1 , taskItem);
+                if(this.IndexOf(taskItem) != 0)
+                {
+                    this.RemoveAt(index);
+                    this.Insert(index - 1 , taskItem);
+                }
+                else
+                {
+                    WriteLine("This task is your number 1 priority. It can't move any higher");
+                }
             }
-            else if(prioritySetting == "lower" && this.IndexOf(taskItem) != (this.Count - 1))
+            else if(prioritySetting == "lower")
             {
-                this.RemoveAt(index);
-                this.Insert(index + 1, taskItem);
+                if(this.IndexOf(taskItem) != (this.Count - 1))
+                {
+                    this.RemoveAt(index);
+                    this.Insert(index + 1, taskItem);
+                }
+                else
+                {
+                    WriteLine("This task is your lowest priority. It can't move any lower");
+                }
             }
             else
-            //FIXME: the rejected scenarios from the higher and lower conditions where the index is out of range 
-            //are falling into here.
-            //they 
             {   
-                Console.WriteLine();
-                Console.WriteLine("Not a valid re-prioritization option");
+                WriteLine();
+                WriteLine("Not a valid re-prioritization option");
             }
         }
     }
