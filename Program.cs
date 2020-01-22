a3faa7afe026f0826df5107fc55f69e191e0990a
evan <evan@evanpavan.com>
2020-01-21 19:42:36 -0500

    Connected database to program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using static ToDoList.TaskHelpers;
 using static System.Console;
+using System.Data.SqlClient;
+using System.Text;
+
 
 namespace ToDoList
 {
@@ -11,183 +14,207 @@ namespace ToDoList
             Tasklist Tasks = new Tasklist{};
             string mainmenuUserinput = "";
 
-            do
+            //Connect to SQL Database
+            try
             {
-                PrintMainMenu();         
-                mainmenuUserinput = ReadLine();
-                if(mainmenuUserinput == "1")
+                string connectString = "Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+                using (SqlConnection connection = new SqlConnection(connectString))
                 {
-                    WriteLine();
-                    WriteLine("Enter a task and hit return or 0 to exit");
-                    string firsttaskUserinput = ReadLine();
-                    if(firsttaskUserinput != "0")
+                    connection.Open();
+                    //Create database
+                    //Remove the Drop database command once up and running so data is not deleted each run of program
+                    //TODO: make drop database an action in the TODO list menu
+                    string sql = "DROP DATABASE IF EXISTS [TodoDB]; CREATE DATABASE [TodoDB]";
+                    using(SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        Tasks.Add(firsttaskUserinput);
-                        string twoOrMoreTasksUserInput = "";
-                        do
+                        command.ExecuteNonQuery();
+                    }
+
+                    //Create the ToDoList Table
+                    StringBuilder strBuilder = new StringBuilder();
+                    strBuilder.Append("USE TodoDB; ");
+                    strBuilder.Append("CREATE TABLE TodoList ( ");
+                    strBuilder.Append("Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
+                    strBuilder.Append("ParentTask TEXT, ");
+                    strBuilder.Append("SubTask1 TEXT, ");
+                    strBuilder.Append("SubTask2 TEXT, ");
+                    strBuilder.Append("SubTask3 TEXT, ");
+                    strBuilder.Append("SubTask4 TEXT, ");
+                    strBuilder.Append("SubTask5 TEXT ");
+                    strBuilder.Append("); ");
+                    sql = strBuilder.ToString();
+
+                    using(SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    do
+                    {
+                        PrintMainMenu();         
+                        mainmenuUserinput = ReadLine();
+                        if(mainmenuUserinput == "1")
                         {
                             WriteLine();
-                            WriteLine("Enter another task and hit return or 0 to exit");
-                            twoOrMoreTasksUserInput = ReadLine();
-
-                            if(twoOrMoreTasksUserInput != "0")                      
+                            WriteLine("Enter a task and hit return or 0 to exit");
+                            string firsttaskUserinput = ReadLine();
+                            if(firsttaskUserinput != "0")
                             {
-                                Tasks.Add(twoOrMoreTasksUserInput);
-                            }
-                        }
-                        while(twoOrMoreTasksUserInput != "0");
-                    }
-                }
-                if(mainmenuUserinput == "2")
-                {      
-                    if(Tasks.TasksIsNull() == false)
-                    {
-                        string showAllTasksInput = "";
-                        do
-                        {
-                            Tasks.PrintAllTasks();
-                            PrintPrioritizeMenu();
-                            showAllTasksInput = ReadLine().ToUpper();
-
-                            MathMethods PriorityDigitparsing = new MathMethods();
-                            int index = PriorityDigitparsing.ParseDigitToIndex(showAllTasksInput);
-
-                            if(PriorityDigitparsing.strippingSuccess == true)
-                            {
-                                string prioritySetting = PrioritySetting(showAllTasksInput);
-                                try
+                                Tasks.Add(firsttaskUserinput);
+                                string twoOrMoreTasksUserInput = "";
+                                do
                                 {
-                                    Tasks.Reprioritize(prioritySetting, index); 
-                                }
-                                catch(System.ArgumentOutOfRangeException)
-                                {
-                                    NoTaskExists(index);
-                                }
-                            }
-                        }
-                        while(showAllTasksInput != "0");                                           
-                    }  
-                }
-                if(mainmenuUserinput == "3")
-                {      
-                    if(Tasks.TasksIsNull() == false)
-                    {
-                        string editTasksInput = "";
-                        do
-                        {
-                            Tasks.PrintAllTasks();
-                            PrintEditMenu();
-                            editTasksInput = ReadLine().ToUpper();
-                            
-                            if(editTasksInput != "0")
-                            {
-                                MathMethods EditingDigitparsing = new MathMethods();
-                                int index = EditingDigitparsing.ParseDigitToIndex(editTasksInput);
+                                    WriteLine();
+                                    WriteLine("Enter another task and hit return or 0 to exit");
+                                    twoOrMoreTasksUserInput = ReadLine();
 
-                                if(EditingDigitparsing.strippingSuccess == true)
-                                {
-                                    if(editTasksInput.Contains("EDIT"))
+                                    if(twoOrMoreTasksUserInput != "0")                      
                                     {
-                                        if(Tasks.ValidateIndex(index) == true)
+                                        Tasks.Add(twoOrMoreTasksUserInput);
+                                    }
+                                }
+                                while(twoOrMoreTasksUserInput != "0");
+                            }
+                        }
+                        if(mainmenuUserinput == "2")
+                        {      
+                            if(Tasks.TasksIsNull() == false)
+                            {
+                                string showAllTasksInput = "";
+                                do
+                                {
+                                    Tasks.PrintAllTasks();
+                                    PrintPrioritizeMenu();
+                                    showAllTasksInput = ReadLine().ToUpper();
+
+                                    MathMethods PriorityDigitparsing = new MathMethods();
+                                    int index = PriorityDigitparsing.ParseDigitToIndex(showAllTasksInput);
+
+                                    if(PriorityDigitparsing.strippingSuccess == true)
+                                    {
+                                        string prioritySetting = PrioritySetting(showAllTasksInput);
+                                        try
                                         {
-                                            PrintEditSubmenu();
-                                            string editResponse = ReadLine();
-                                            if(editResponse != "0")
-                                            {
-                                                try
-                                                {
-                                                    Tasks[index] = editResponse;
-                                                }
-                                                catch(System.ArgumentOutOfRangeException)
-                                                {
-                                                    NoTaskExists(index);
-                                                }
-                                            }
+                                            Tasks.Reprioritize(prioritySetting, index); 
                                         }
-                                        else
+                                        catch(System.ArgumentOutOfRangeException)
                                         {
                                             NoTaskExists(index);
                                         }
                                     }
-                                    else
+                                }
+                                while(showAllTasksInput != "0");                                           
+                            }  
+                        }
+                        if(mainmenuUserinput == "3")
+                        {      
+                            if(Tasks.TasksIsNull() == false)
+                            {
+                                string editTasksInput = "";
+                                do
+                                {
+                                    Tasks.PrintAllTasks();
+                                    PrintEditMenu();
+                                    editTasksInput = ReadLine().ToUpper();
+                                    
+                                    if(editTasksInput != "0")
                                     {
-                                        WriteLine();
-                                        WriteLine("Not a valid option entered");
+                                        MathMethods EditingDigitparsing = new MathMethods();
+                                        int index = EditingDigitparsing.ParseDigitToIndex(editTasksInput);
+
+                                        if(EditingDigitparsing.strippingSuccess == true)
+                                        {
+                                            if(editTasksInput.Contains("EDIT"))
+                                            {
+                                                if(Tasks.ValidateIndex(index) == true)
+                                                {
+                                                    PrintEditSubmenu();
+                                                    string editResponse = ReadLine();
+                                                    if(editResponse != "0")
+                                                    {
+                                                        try
+                                                        {
+                                                            Tasks[index] = editResponse;
+                                                        }
+                                                        catch(System.ArgumentOutOfRangeException)
+                                                        {
+                                                            NoTaskExists(index);
+                                                        }
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    NoTaskExists(index);
+                                                }
+                                            }
+                                            else
+                                            {
+                                                WriteLine();
+                                                WriteLine("Not a valid option entered");
+                                            }
+                                        }
                                     }
                                 }
-                            }
+                                while(editTasksInput != "0");
+                            } 
                         }
-                        while(editTasksInput != "0");
-                    } 
-                }
-                if(mainmenuUserinput == "4")
-                {
-                    if(Tasks.TasksIsNull() == false)
-                    {
-                        string subTasksInput = "";
-                        do
+                        if(mainmenuUserinput == "4")
                         {
-                            Tasks.PrintAllTasks();
-                            PrintSubTasksMenu();
-                            subTasksInput = ReadLine().ToUpper();
-
-                            MathMethods SubTaskDigitparsing = new MathMethods();
-                            int index = SubTaskDigitparsing.ParseDigitToIndex(subTasksInput);
-
-                            if(SubTaskDigitparsing.strippingSuccess == true)
+                            if(Tasks.TasksIsNull() == false)
                             {
-                                if(subTasksInput.Contains("SUB"))
+                                string subTasksInput = "";
+                                do
                                 {
-                                    if(Tasks.ValidateIndex(index) == true)
+                                    Tasks.PrintAllTasks();
+                                    PrintSubTasksMenu();
+                                    subTasksInput = ReadLine().ToUpper();
+
+                                    MathMethods SubTaskDigitparsing = new MathMethods();
+                                    int index = SubTaskDigitparsing.ParseDigitToIndex(subTasksInput);
+
+                                    if(SubTaskDigitparsing.strippingSuccess == true)
                                     {
-                                        PrintSubTasksSubMenu();
-                                        string subTaskResponse = ReadLine();
-                                        if(subTaskResponse != "0")
+                                        if(subTasksInput.Contains("SUB"))
                                         {
-                                            try
+                                            if(Tasks.ValidateIndex(index) == true)
                                             {
-                                                //create a list within the list
-                                                //this actually may eff up everything else because now the object
-                                                //is no longer a list it's a list of lists
-                                                //let's play it out and see
-                                                //otherwise we will need to change the class
-                                                //and initialize it as a list of lists.
-                                                
-                                                //IF no subtasks exists
-                                                //create a list and initialize it with the task as index 0
-                                                //and the sub-task as 1
-                                                //insert list into the index that the current member is at
-
-                                                //the list will contain the task as 
-
-                                                //IF subtasks exist then add the new subtask as the last item
-
-                                                //Pretty sure this is going to eff everything up.
-
+                                                PrintSubTasksSubMenu();
+                                                string subTaskResponse = ReadLine();
+                                                if(subTaskResponse != "0")
+                                                {
+                                                    try
+                                                    {
+ 
+                                                    }
+                                                    catch(System.ArgumentOutOfRangeException)
+                                                    {
+                                                        NoTaskExists(index);
+                                                    }
+                                                }
                                             }
-                                            catch(System.ArgumentOutOfRangeException)
+                                            else
                                             {
                                                 NoTaskExists(index);
                                             }
                                         }
-                                    }
-                                    else
-                                    {
-                                        NoTaskExists(index);
+                                        else
+                                        {
+                                            WriteLine();
+                                            WriteLine("Not a valid option entered");
+                                        }
                                     }
                                 }
-                                else
-                                {
-                                    WriteLine();
-                                    WriteLine("Not a valid option entered");
-                                }
-                            }
+                                while(subTasksInput != "0");                                           
+                            }  
                         }
-                        while(subTasksInput != "0");                                           
-                    }  
+                    }
+                    while(mainmenuUserinput != "9");
                 }
             }
-            while(mainmenuUserinput != "9");
+            catch(SqlException exception)
+            {
+                WriteLine(exception.ToString());
+            }
         }
     }
 }
